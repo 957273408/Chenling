4d8bffcd478417d18d8c09492af6c9920d2111fa
north summer <957273408@qq.com>
2019-06-13 01:22:12 +0800

    22

--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -153,24 +153,51 @@
           <div class="item">
             <img src=""
                  alt="">
-            <p>疑难杂症</p>
-            <span>146487人已加入</span>
-            <router-link to="">
-              <button>
-                进入社群
-              </button>
+            <div>
+              <p>疑难杂症</p>
+              <span>146487人已加入</span>
+            </div>
+            <router-link to=""
+                         tag="button">
+              进入社群
             </router-link>
           </div>
           <div class="item">
             <img src=""
                  alt="">
-            <p>疑难杂症</p>
-            <span>146487人已加入</span>
-            <router-link to="">
-              <button>
-                进入社群
-              </button>
+            <div>
+              <p>疑难杂症</p>
+              <span>146487人已加入</span>
+            </div>
+            <router-link to=""
+                         tag="button">
+              进入社群
             </router-link>
+          </div>
+
+        </div>
+      </section>
+      <section class="pharmacy">
+        <div class="top">
+          <p class="left"><span>亿长寿</span>大药房</p>
+          <router-link to=""
+                       slot="right">
+            更多
+          </router-link>
+        </div>
+        <div class="content">
+          <div class="item">
+            <div></div>
+            <div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+          <div class="item">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
           </div>
         </div>
       </section>
@@ -195,143 +222,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import "@/assets/css/mixin.scss";
-header {
-  .van-swipe {
-    width: 100%;
-    height: 341px;
-    .van-swipe-item {
-      img {
-        width: 100%;
-        height: 100%;
-      }
-    }
-  }
-  /deep/ .van-swipe__indicators {
-    left: 619px;
-    bottom: 45px;
-  }
-  /deep/ .van-search {
-    position: absolute;
-    top: 16px;
-    width: 100%;
-    .van-cell {
-      background: rgba(255, 255, 255, 1);
-      opacity: 0.8;
-      border-radius: 32px;
-    }
-  }
-}
-
-nav {
-  position: absolute;
-  overflow: hidden;
-  left: 50%;
-  transform: translate(-50%, -41px);
-  width: 688px;
-  height: 280px;
-  background: rgba(255, 255, 255, 1);
-  box-shadow: 0px 2px 8px 0px rgba(0, 0, 0, 0.05);
-  border-radius: 20px;
-  .item {
-    width: 50%;
-    box-sizing: border-box;
-    padding: 0 30px;
-    background-color: yellow;
-    p {
-      font-size: 22px;
-      font-family: SourceHanSansCN-Regular;
-      font-weight: 400;
-      width: 310px;
-      color: rgba(145, 147, 157, 1);
-    }
-    .icon {
-      display: inline-block;
-      width: 39px;
-      height: 41px;
-      background: linear-gradient(
-        142deg,
-        rgba(247, 150, 93, 1),
-        rgba(255, 195, 123, 1)
-      );
-      box-shadow: 0px 2px 4px 0px rgba(248, 153, 95, 0.4);
-    }
-    .title {
-      margin: 25px 0 5px 0;
-      text-align: left;
-      font-size: 32px;
-      font-family: SourceHanSansCN-Bold;
-      font-weight: bold;
-      color: rgba(0, 0, 0, 1);
-    }
-  }
-}
-main {
-  padding: 0 31px;
-}
-.menu {
-  padding: 240px 36px 0;
-  @include fj("center");
-  flex-wrap: wrap;
-  .item {
-    @include fj(center);
-    flex-direction: column;
-    align-items: center;
-    @include wh(25%, 200px);
-    img {
-      width: 102px;
-      height: 76px;
-    }
-    p {
-      @include sc(22px, rgba(73, 73, 73, 1));
-      margin-top: 9px;
-    }
-  }
-}
-.banner {
-  @include wh(688px, auto);
-  margin: 20px auto 0;
-  img {
-    @include wh(100%, 100%);
-  }
-}
-.happly-go {
-  @include wh(688px, 457px);
-  position: relative;
-  background: linear-gradient(
-    90deg,
-    rgba(233, 79, 71, 1),
-    rgba(235, 101, 94, 1)
-  );
-  box-shadow: 0 11px 10px rgba(233, 79, 71, 0.1);
-  margin: 40px auto 0;
-  border-radius: 20px;
-  .top {
-    height: 84px;
-    @include fj(space-between);
-    padding: 0 30px;
-    align-items: center;
-  }
-  .van-swipe {
-    width: 100%;
-    height: 370px;
-    .van-swipe-item {
-      img {
-        width: 100%;
-        height: 100%;
-      }
-    }
-  }
-}
-.hserve {
-  .content {
-    @include fj();
-    flex-wrap: wrap;
-    .item {
-      @include wh(336px, 160px);
-      background-color: red;
-      margin-top: 15px;
-    }
-  }
-}
+@import "./css/index.scss";
 </style>
